4f648cc62087d2e6e58bc8d85de00d7ecd89beab
cristinagiacobbe <crigiak@gmail.com>
2024-01-30 23:00:56 +0100

    Create main in component MainApp.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,20 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <main>
+        <h3>Content goes here </h3>
+    </main>
+</template>
 
-<style></style>
+<style>
+main {
+    background-image: url('/fondo-pag-speciali.jpg');
+    min-height: 200px;
+}
+
+h3 {
+    text-align: center;
+    line-height: 200px;
+}
+</style>
