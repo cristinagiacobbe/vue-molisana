3725e47fa57941b0b0861c7ff7999cf0e651b167
cristinagiacobbe <crigiak@gmail.com>
2024-01-30 22:44:04 +0100

    Create header in component AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+    name: 'AppHeader',
+
+
+}
+
+</script>
+
+<template>
+    <header>
+        <img src="/la-molisana-logo.png" alt="">
+        <nav>
+            <ul class="header_menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Contatti</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+header {
+    text-align: center;
+}
+
+.header_menu {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+li {
+    list-style: none;
+}
+</style>
