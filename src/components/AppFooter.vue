119257f29992f882e6f0ded8c6020684aaaa7c23
cristinagiacobbe <crigiak@gmail.com>
2024-01-30 23:12:16 +0100

    Create footer in component AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,6 +8,46 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="/la-molisana-logo.png" alt="">
+            </div>
+            <div class="col">
+                <ul>
+                    <li>
+                        <h4>PASTIFICIO</h4>
+                    </li>
+                    <li>Il pastificio</li>
+                    <li>Grano</li>
+                    <li>Filiera</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                </ul>
+            </div>
+            <div class="col">
+                <li>
+                    <h4>PRODOTTI</h4>
+                </li>
+                <li>Le classiche</li>
+                <li>Le integrali</li>
+                <li>Le speciali</li>
+                <li>Le biologiche</li>
+                <li>Le gluten-free</li>
+            </div>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style>
+.container {
+    width: 60%;
+    margin: auto;
+}
+
+.row {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
